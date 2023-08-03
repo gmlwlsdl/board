70471b2fcc0cb67aced413f17d2caa95d2fcaa69
gmlwlsdl <heejin094@naver.com>
2023-08-03 17:32:55 +0900

    !

--- signup.php
+++ signup.php
@@ -4,16 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>signup</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
-<body>
+<body style="background-color: #DBDCEF;">
+<center>
     <h2>Welcome!</h2>
     <form method="get" action="signup-check.php">
-        <p><label>ID <input type="text" name="ID"></label></p>
-        <p><label>PW <input type="text" name="PW"></label></p>
-        <p><label>R_PW <input type="text" name="R_PW"></label></p>
-        <p><label>NAME <input type="text" name="NAME"></label></p>
-        <p><label>Email <input type="text" name="Email"></label></p>
-        <p><input type="submit" value="Submit"></p>   
-    </form>
+        <p><label>ID <input type="text" id="input" name="ID"></label></p>
+        <p><label>PW <input type="text" id="input" name="PW"></label></p>
+        <p><label>R_PW <input type="text" id="input" name="R_PW"></label></p>
+        <p><label>NAME <input type="text" id="input" name="NAME"></label></p>
+        <p><label>Email <input type="text" id="input" name="Email"></label></p>
+        <p><input type="submit" id="b2" value="Submit"></p>   
+    </form></center>
+    <center>
+            <a href="index.php">이전으로</a> 
+    </center>
 </body>
 </html>
